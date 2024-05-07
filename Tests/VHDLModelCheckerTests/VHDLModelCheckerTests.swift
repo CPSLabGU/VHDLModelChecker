f674a55fece27e858bdd5fbf7c9c8f0146012148
Morgan McColl <morgan.mccoll92@gmail.com>
2024-05-08 07:13:13 +1000

    Add test against RecoveryMode kripke structure.

--- Tests/VHDLModelCheckerTests/VHDLModelCheckerTests.swift
+++ Tests/VHDLModelCheckerTests/VHDLModelCheckerTests.swift
@@ -1,11 +1,30 @@
+import Foundation
+import TCTLParser
+import VHDLKripkeStructures
 @testable import VHDLModelChecker
 import XCTest
 
+/// Test class for ``VHDLModelChecker``.
 final class VHDLModelCheckerTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        // XCTAssertEqual(VHDLModelChecker().text, "Hello, World!")
+
+    /// Test a basic verification.
+    func testBasicVerification() throws {
+        let specRaw = """
+        // spec:language VHDL
+
+        A G RecoveryModeMachine_failureCount = 3 -> recoveryMode = '1'
+        """
+        guard let specification = Specification(rawValue: specRaw) else {
+            XCTFail("Failed to create spec.")
+            return
+        }
+        let path = FileManager.default.currentDirectoryPath.appending(
+            "/Tests/VHDLModelCheckerTests/output.json"
+        )
+        let data = try Data(contentsOf: URL(fileURLWithPath: path, isDirectory: false))
+        let decoder = JSONDecoder()
+        let kripkeStructure = try decoder.decode(KripkeStructure.self, from: data)
+        let modelChecker = VHDLModelChecker(structure: kripkeStructure)
+        _ = try modelChecker.verify(against: specification)
     }
 }
