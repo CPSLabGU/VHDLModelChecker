255b292e0775bfc708a349093279b2e6e0d82aa0
Morgan McColl <morgan.mccoll92@gmail.com>
2024-08-15 09:22:49 +1000

    Added new C-target in.

--- Package.swift
+++ Package.swift
@@ -22,20 +22,22 @@ let package = Package(
         .package(url: "https://github.com/mipalgu/VHDLParsing", from: "2.4.0"),
         .package(url: "https://github.com/cpslabgu/TCTLParser", from: "1.1.2"),
         .package(url: "https://github.com/cpslabgu/VHDLKripkeStructures", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
+        // .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package
         // depends on.
+        .systemLibrary(name: "CSQLite", pkgConfig: "sqlite3"),
         .target(
             name: "VHDLModelChecker",
             dependencies: [
                 .product(name: "VHDLParsing", package: "VHDLParsing"),
                 .product(name: "TCTLParser", package: "TCTLParser"),
                 .product(name: "VHDLKripkeStructures", package: "VHDLKripkeStructures"),
-                .product(name: "SQLite", package: "SQLite.swift")
+                // .product(name: "SQLite", package: "SQLite.swift")
+                .target(name: "CSQLite")
             ]
         ),
         .executableTarget(
@@ -53,7 +55,8 @@ let package = Package(
                 .product(name: "VHDLParsing", package: "VHDLParsing"),
                 .product(name: "TCTLParser", package: "TCTLParser"),
                 .product(name: "VHDLKripkeStructures", package: "VHDLKripkeStructures"),
-                .product(name: "SQLite", package: "SQLite.swift")
+                // .product(name: "SQLite", package: "SQLite.swift")
+                .target(name: "CSQLite")
             ],
             resources: [
                 .process("output.json"),
